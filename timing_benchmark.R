481869a8f74233bdd16cb58abbfb42ffb664b958
iferres <igferres@gmail.com>
2021-01-21 13:15:24 -0300

    added benchmark script

---
+++ timing_benchmark.R
@@ -0,0 +1,142 @@
+# setwd("/mnt/4tera/iferres/benchmark_pagoo")
+run_roary <- function(gffs, p = 1, f = ".", singularity_image = "containers/roary.sif"){
+  gffs <- normalizePath(gffs)
+  cmd <- paste(
+    "singularity",
+    "exec",
+    "--bind", getwd(),
+    singularity_image,
+    "roary",
+    "-p", p,
+    "-f", f,
+    paste(gffs, collapse = " "),
+    sep = " "
+    )
+  system(cmd)
+  list.files(path = f, full.names = TRUE)
+}
+
+
+
+generate_input <- function(gene_presence_absence_csv, gffs, sep = "__"){
+	library(Biostrings)
+  df <- read.csv(gene_presence_absence_csv, header = TRUE,
+                 sep = ",", stringsAsFactors = FALSE, check.names = FALSE)
+  cluster_meta <- df[, c("Gene", "Annotation")]
+  colnames(cluster_meta) <- c("cluster", "Annotation")
+  dims <- dim(df)
+  df <- df[, 15:dims[2]]
+  lp <- lapply(df, strsplit, "\t")
+  lp <- lapply(lp, setNames, cluster_meta$cluster)
+  lths <- lapply(lp, sapply, length)
+  sums <- sapply(lths, sum)
+  mm <- cbind(unlist(lp, use.names = FALSE), unlist(lapply(lths,
+                                                           function(x) {
+                                                             unlist(rep(names(x), x), use.names = FALSE)
+                                                           }), use.names = FALSE), rep(names(sums), sums))
+  mm <- as.data.frame(mm)
+  colnames(mm) <- c("gene", "cluster", "org")
+  names(gffs) <- sub("[.]gff$", "", basename(gffs))
+  seqs <- lapply(gffs, function(x) {
+    sq <- pagoo:::read_gff(x)
+    sq <- sq[which(names(sq) %in% mm$gene)]
+  })
+  mcls <- lapply(seqs, mcols)
+  mcls <- lapply(names(mcls), function(x) {
+    mcls[[x]]$org <- x
+    mcls[[x]]
+  })
+  cols <- c("seqid", "type", "start", "end", "strand",
+            "product", "org", "locus_tag")
+  mcls <- lapply(mcls, function(x) x[, cols])
+  mcls <- do.call(rbind, mcls)
+  ma <- match(paste(mm$org, mm$gene, sep = sep), paste(mcls$org,
+                                                       mcls$locus_tag, sep = sep))
+  mm <- cbind(mm, mcls[ma, c("seqid", "type", "start",
+                             "end", "strand", "product")])
+
+  return(list(data = mm, cluster_meta = cluster_meta, sequences = DNAStringSetList(seqs), sep = sep))
+}
+
+
+
+
+
+
+allgffs <- list.files(path = "decano_downing_2019_dataset_gffs/", pattern = "[.]gff$", full.names = TRUE)
+
+library(pagoo)
+library(parallel)
+ns <- c(10, 100, 500, 1000)
+fout <- paste("N", ns, sep = "_")
+set.seed(123)
+gffs <- lapply(ns, function(x) sample(allgffs, x) )
+
+
+timings_dfs <- mcmapply(function(gffs, n, f, singularity_image, p) {
+
+  # gffs <- sample(allgffs, n)
+  # roary_out <- run_roary(gffs, p = p, f = f, singularity_image = singularity_image)
+  roary_out <- list.files(path = f, full.names = TRUE)
+  gpa <- grep("gene_presence_absence[.]csv$", roary_out, value = TRUE)
+
+  dfout <- lapply(1:10, function(x){
+    st1 <- system.time(p <- roary_2_pagoo(gpa))
+    df1 <- data.frame(Operation = "roary_csv",
+                      N = n,
+                      Dir = f,
+                      User = st1[["user.self"]],
+                      System = st1[["sys.self"]],
+                      Elapsed = st1[["elapsed"]])
+
+    st2 <- system.time(p$gg_barplot())
+    df2 <- data.frame(Operation = "barplot",
+                      N = n,
+                      Dir = f,
+                      User = st2[["user.self"]],
+                      System = st2[["sys.self"]],
+                      Elapsed = st2[["elapsed"]])
+
+    st3 <- system.time(p <- roary_2_pagoo(gpa, gffs))
+    df3 <- data.frame(Operation = "roary_csv_gff",
+                      N = n,
+                      Dir = f,
+                      User = st3[["user.self"]],
+                      System = st3[["sys.self"]],
+                      Elapsed = st3[["elapsed"]])
+
+    st4 <- system.time(p$core_seqs_4_phylo())
+    df4 <- data.frame(Operation = "core_seq",
+                      N = n,
+                      Dir = f,
+                      User = st4[["user.self"]],
+                      System = st4[["sys.self"]],
+                      Elapsed = st4[["elapsed"]])
+    rm(p)
+
+
+
+    input <- generate_input(gpa, gffs)
+    st5 <- system.time(pagoo(data = input$data, cluster_meta = input$cluster_meta,
+                                  sequences = input$sequences, sep = input$sep))
+    df5 <- data.frame(Operation = "preloaded_seq",
+                      N = n,
+                      Dir = f,
+                      User = st5[["user.self"]],
+                      System = st5[["sys.self"]],
+                      Elapsed = st5[["elapsed"]])
+
+    rbind(df1, df2, df3, df4, df5)
+  })
+  return(dfout)
+
+}, gffs = gffs, n = ns, f = fout, MoreArgs = list(singularity_image = "containers/roary.sif", p = 6), SIMPLIFY = FALSE, mc.cores = 5)
+saveRDS(timings_dfs, file = "timings.RDS")
+
+x <- do.call(rbind, unlist(timings_dfs, recursive = FALSE))
+write.table(x, file = "timings.tsv", quote = FALSE, sep = "\t", row.names = FALSE)
+
+library(ggplot2)
+g <- ggplot(x, aes(y=Elapsed, x=N, color=Operation, group=N)) + geom_point() + facet_wrap(~Type, scales="free_y", nrow=2) + stat_summary(fun=median, geom="line", aes(group=Operation)) + theme_light() + scale_color_discrete(labels=c("$gg_barplot()", "$core_seqs_4_phylo()", "pagoo()", "roary_2_pagoo()", "roary_2_pagoo() + gff"), guide = guide_legend(reverse=T)) + ylab("Elapsed time (s)") + xlab("Number of organisms")
+
+ggsave("bench_pagoo.pdf", plot=g)
